dfef2a899ce9c6b5cd235b1c7ad375b353af62b2
Dheeraj-Manwani <dkmanwani2000@gmail.com>
2025-11-26 11:46:07 +0530

    added new roles to admin screen

--- app/admin/access/page.tsx
+++ app/admin/access/page.tsx
@@ -53,6 +53,11 @@ const assignableRoles: Role[] = [
   "FOREST_OFFICE_GONDIA",
   "SAMAJ_KALYAN_OFFICE_GONDIA",
   "SLR_OFFICE_GONDIA",
+  // New Departments Roles added: 24-11-2025
+  "RDC_GONDIA",
+  "DEPUTY_COLLECTOR_GENERAL_GONDIA",
+  "PO_OFFICE_DEORI",
+  "ST_DEPO_OFFICE_GONDIA",
 ];
 
 export default function AdminAccessPage() {
